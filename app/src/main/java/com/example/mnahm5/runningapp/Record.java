5afdce0326926d16ef48357797c826c19bad66b1
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-06-25 13:18:51 +1000

    Almost Done with Record Activity

--- app/src/main/java/com/example/mnahm5/runningapp/Record.java
+++ app/src/main/java/com/example/mnahm5/runningapp/Record.java
@@ -23,7 +23,9 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.fitness.data.Application;
@@ -34,16 +36,27 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+import com.google.android.gms.vision.text.Text;
+
+import java.util.ArrayList;
 
 public class Record extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private final int REQUEST_PERMISSION_GPS = 1;
     private int count = 0;
-    private String btMsg;
-    private Marker startMarker = null;
-    private Marker endMarker = null;
     private boolean recordIndicator = false;
+    private TextView tvAccuracy, tvDistance, tvTime;
+    private Button btTop, btMiddle, btBottom;
+    private ArrayList<ArrayList<LatLng>> polylinePointsList = new ArrayList<ArrayList<LatLng>>();
+    private ArrayList<Polyline> polylineArrayList = new ArrayList<Polyline>();
+    private ArrayList<Marker> markerArrayList = new ArrayList<Marker>();
+    private String markerTitle;
+    private int noOfPolylines;
 
     @Override
     public Context getApplicationContext() {
@@ -59,34 +72,28 @@ public class Record extends AppCompatActivity implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        final Button btTop = (Button) findViewById(R.id.btTop);
-        final Button btMiddle = (Button) findViewById(R.id.btMiddle);
-        final Button btBottom = (Button) findViewById(R.id.btBottom);
-
-        btTop.setVisibility(View.GONE);
-        btMiddle.setVisibility(View.GONE);
+        btTop = (Button) findViewById(R.id.btTop);
+        btMiddle = (Button) findViewById(R.id.btMiddle);
+        btBottom = (Button) findViewById(R.id.btBottom);
+        tvAccuracy = (TextView) findViewById(R.id.tvAccuracy);
+        tvDistance = (TextView) findViewById(R.id.tvDistance);
+        tvTime = (TextView) findViewById(R.id.tvTime);
 
         Toast.makeText(Record.this,"Please Wait while we pinpoint your location",Toast.LENGTH_SHORT).show();
+        changeToState1();
 
         btBottom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (count == 0) {
-                    recordIndicator = true;
-                    btMsg = "Pause/Stop";
-                    btBottom.setText(btMsg);
+                    changeToState2();
                 }
                 else if (count == 1) {
-                    recordIndicator = false;
-                    btMsg = "Save";
-                    btBottom.setText(btMsg);
-                    btMiddle.setVisibility(View.VISIBLE);
-                    btTop.setVisibility(View.VISIBLE);
+                    changeToState3();
                 }
                 else if (count == 2) {
                     Toast.makeText(Record.this,"Count = 2",Toast.LENGTH_SHORT).show();
                 }
-                count++;
             }
         });
 
@@ -98,16 +105,13 @@ public class Record extends AppCompatActivity implements OnMapReadyCallback {
                         .setMessage("Are you sure you want to delete this entry?")
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
+                                changeToState1();
                                 Toast.makeText(Record.this,"Map Cleared",Toast.LENGTH_SHORT).show();
-                                btTop.setVisibility(View.GONE);
-                                btMiddle.setVisibility(View.GONE);
-                                btMsg = "Start";
-                                btBottom.setText(btMsg);
                             }
                         })
                         .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(Record.this,"Not Gonna Clear",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(Record.this,"Run not cleared",Toast.LENGTH_SHORT).show();
                             }
                         })
                         .setIcon(android.R.drawable.ic_dialog_alert)
@@ -118,10 +122,7 @@ public class Record extends AppCompatActivity implements OnMapReadyCallback {
         btTop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btTop.setVisibility(View.GONE);
-                btMiddle.setVisibility(View.GONE);
-                btMsg = "Pause/Stop";
-                btBottom.setText(btMsg);
+                changeToState2();
             }
         });
     }
@@ -143,32 +144,48 @@ public class Record extends AppCompatActivity implements OnMapReadyCallback {
             }
         }
         else {
-            Toast.makeText(Record.this, "Permission Granted", Toast.LENGTH_SHORT).show();
             LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
             LocationListener locationListener = new LocationListener() {
                 @Override
                 public void onLocationChanged(Location location) {
+                    String msg = "Accuracy: " + location.getAccuracy();
+                    tvAccuracy.setText(msg);
                     if (!recordIndicator) {
                         LatLng currentLocation = new LatLng(location.getLatitude(),location.getLongitude());
-                        if (startMarker != null) {
-                            startMarker.remove();
+                        if (noOfPolylines < markerArrayList.size()) {
+                            markerArrayList.get(noOfPolylines).remove();
+                            Marker newMarker = mMap.addMarker(new MarkerOptions()
+                                    .position(currentLocation)
+                                    .title(markerTitle));
+                            markerArrayList.set(noOfPolylines,newMarker);
                         }
-                        startMarker = mMap.addMarker(new MarkerOptions()
-                            .position(currentLocation)
-                            .title("Start Position"));
+                        else {
+                            Marker newMarker = mMap.addMarker(new MarkerOptions()
+                                    .position(currentLocation)
+                                    .title(markerTitle));
+                            markerArrayList.add(noOfPolylines,newMarker);
+                        }
+                        mMap.animateCamera(CameraUpdateFactory.zoomTo(25));
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
-                        mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
                     }
                     else {
                         LatLng currentLocation = new LatLng(location.getLatitude(),location.getLongitude());
-                        if (endMarker != null) {
-                            endMarker.remove();
+                        if (noOfPolylines < (markerArrayList.size() - 1)) {
+                            markerArrayList.get(noOfPolylines + 1).remove();
+                            Marker newMarker = mMap.addMarker(new MarkerOptions()
+                                    .position(currentLocation)
+                                    .title(markerTitle));
+                            markerArrayList.set(noOfPolylines + 1, newMarker);
                         }
-                        mMap.animateCamera(CameraUpdateFactory.zoomTo(16));
-                        endMarker = mMap.addMarker(new MarkerOptions()
-                                .position(currentLocation)
-                                .title("Start Position"));
+                        else {
+                            Marker newMarker = mMap.addMarker(new MarkerOptions()
+                                    .position(currentLocation)
+                                    .title(markerTitle));
+                            markerArrayList.add(noOfPolylines + 1, newMarker);
+                        }
+                        mMap.animateCamera(CameraUpdateFactory.zoomTo(25));
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                        drawPolyline();
                     }
                 }
 
@@ -195,6 +212,97 @@ public class Record extends AppCompatActivity implements OnMapReadyCallback {
         }
     }
 
+    public void drawPolyline() {
+        Marker startMarker, endMarker;
+        Polyline polyline;
+        ArrayList<LatLng> points;
+        if (noOfPolylines < polylinePointsList.size()) {
+            points = polylinePointsList.get(noOfPolylines);
+        }
+        else {
+            points = new ArrayList<LatLng>();
+        }
+        if (noOfPolylines < markerArrayList.size() && noOfPolylines < polylinePointsList.size()) {
+            startMarker = markerArrayList.get(noOfPolylines);
+            startMarker.setTitle("Start Position");
+            LatLng startPoint = new LatLng(startMarker.getPosition().latitude, startMarker.getPosition().longitude);
+            points.add(startPoint);
+        }
+        if (noOfPolylines < (markerArrayList.size() - 1) && noOfPolylines < polylinePointsList.size()) {
+            endMarker = markerArrayList.get(noOfPolylines + 1);
+            LatLng endPoint = new LatLng(endMarker.getPosition().latitude, endMarker.getPosition().longitude);
+            points.add(endPoint);
+        }
+        if (noOfPolylines < polylineArrayList.size()) {
+            polyline = polylineArrayList.get(noOfPolylines);
+            polyline.remove();
+        }
+//        else {
+//            LatLng startPoint = new LatLng(startMarker.getPosition().latitude, startMarker.getPosition().longitude);
+//            polylinePoints.add(startPoint);
+//        }
+//        LatLng endPoint = new LatLng(endMarker.getPosition().latitude, endMarker.getPosition().longitude);
+//        polylinePoints.add(endPoint);
+        PolylineOptions polylineOptions = new PolylineOptions();
+        for (int i = 0; i < points.size(); i++) {
+            LatLng point = points.get(i);
+            polylineOptions.add(point);
+        }
+        polyline = mMap.addPolyline(polylineOptions);
+        if (noOfPolylines < polylineArrayList.size()) {
+            polylineArrayList.set(noOfPolylines, polyline);
+        }
+        else {
+            polylineArrayList.add(noOfPolylines, polyline);
+        }
+        if (noOfPolylines < polylinePointsList.size()) {
+            polylinePointsList.set(noOfPolylines, points);
+        }
+        else {
+            polylinePointsList.add(noOfPolylines, points);
+        }
+    }
+
+    public void changeToState1() {
+        markerTitle = "Current Position";
+        recordIndicator = false;
+        count = 0;
+        noOfPolylines = 0;
+        polylinePointsList.clear();
+        polylineArrayList.clear();
+        markerArrayList.clear();
+        String btMsg = "Start";
+        btBottom.setText(btMsg);
+        btTop.setVisibility(View.GONE);
+        btMiddle.setVisibility(View.GONE);
+        if (mMap != null) {
+            mMap.clear();
+        }
+    }
+
+    public void changeToState2() {
+        markerTitle = "End Position";
+        recordIndicator = true;
+        count = 1;
+//        noOfPolylines++;
+        polylinePointsList.add(new ArrayList<LatLng>());
+        String btMsg = "Pause/Stop";
+        btBottom.setText(btMsg);
+        btTop.setVisibility(View.GONE);
+        btMiddle.setVisibility(View.GONE);
+    }
+
+    public void changeToState3() {
+        markerTitle = "Current Position";
+        recordIndicator = false;
+        count = 2;
+        noOfPolylines++;
+        String btMsg = "Save";
+        btBottom.setText(btMsg);
+        btMiddle.setVisibility(View.VISIBLE);
+        btTop.setVisibility(View.VISIBLE);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
@@ -207,5 +315,4 @@ public class Record extends AppCompatActivity implements OnMapReadyCallback {
                 }
         }
     }
-
 }
